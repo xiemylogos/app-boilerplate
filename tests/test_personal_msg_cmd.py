018e5528e0f6bb3d646a29e975aea59a59740377
xiemylogos <xiemylogos@gmail.com>
2025-01-21 14:14:50 +0800

    update personal msg unit test verify

--- tests/test_personal_msg_cmd.py
+++ tests/test_personal_msg_cmd.py
@@ -2,7 +2,8 @@ import pytest
 
 from application_client.boilerplate_personal_msg import PersonalMsg
 from application_client.boilerplate_command_sender import BoilerplateCommandSender, Errors
-from application_client.boilerplate_response_unpacker import unpack_get_public_key_response, unpack_sign_personal_msg_response
+from application_client.boilerplate_response_unpacker import unpack_get_public_key_response, \
+    unpack_sign_personal_msg_response
 from ragger.error import ExceptionRAPDU
 from utils import check_signature_validity
 from utils import checkpersonal_signature_validity
@@ -14,6 +15,8 @@ SIGN_MAGIC = b"\x19Ontology Signed Message:\n"
 # Configure logger
 logger = logging.getLogger("test_logger")
 logger.setLevel(logging.DEBUG)
+
+
 # In this tests we check the behavior of the device when asked to sign a personal msg
 
 
@@ -31,8 +34,9 @@ def test_sign_personal_msg_short_msg(backend, scenario_navigator):
     _, public_key, _, _ = unpack_get_public_key_response(rapdu.data)
     logger.debug("pubkey:%s",public_key.hex())
     # Create the personalmsg that will be sent to the device for signing
+    str_personal_msg = "test message"
     personalmsg = PersonalMsg(
-        personalmsg="test message"
+        personalmsg=str_personal_msg
     ).serialize()
     logger.debug("personal msg:%s",personalmsg.hex())
 
@@ -42,16 +46,16 @@ def test_sign_personal_msg_short_msg(backend, scenario_navigator):
     with client.sign_personal_msg(path=path, personalmsg=personalmsg):
         # Validate the on-screen request by performing the navigation appropriate for this device
         scenario_navigator.review_approve()
-    
-    personalmsg = SIGN_MAGIC[:-1]+int_byte(utf8_strlen("test message"))+personalmsg
+
+    personalmsg = SIGN_MAGIC + str(utf8_strlen(str_personal_msg)).encode() + personalmsg
     # The device as yielded the result, parse it and ensure that the signature is correct
     response = client.get_async_response().data
     _, der_sig, _ = unpack_sign_personal_msg_response(response)
-    logger.debug("sig:%s",der_sig.hex())
-    logger.debug("personalmsg len:%d",len(personalmsg))
-    logger.debug("pubkey:%s,der_sig:%s,personalmsg:%s",public_key.hex(),der_sig.hex(),personalmsg.hex())
-   # assert checkpersonal_signature_validity(public_key, der_sig, personalmsg)
-    #assert len(der_sig) == 72
+    logger.debug("sig:%s", der_sig.hex())
+    logger.debug("msg len:%d", utf8_strlen(str_personal_msg))
+    logger.debug("personalmsg len:%d", len(personalmsg))
+    logger.debug("pubkey:%s,der_sig:%s,personalmsg:%s", public_key.hex(), der_sig.hex(), personalmsg.hex())
+    assert checkpersonal_signature_validity(public_key, der_sig, personalmsg)
 
 
 def test_sign_personal_msg_long_msg(backend, scenario_navigator):
@@ -61,16 +65,18 @@ def test_sign_personal_msg_long_msg(backend, scenario_navigator):
 
     rapdu = client.get_public_key(path=path)
     _, public_key, _, _ = unpack_get_public_key_response(rapdu.data)
-
+    str_personal_msg = "This is a very long personal msg. "
+    "It will force the app client to send the serialized personal msg to be sent in chunk. "
+    "As the maximum chunk size is 255 bytes we will make this memo greater than 255 characters. "
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam."
     personalmsg = PersonalMsg(
-        personalmsg=("This is a very long personal msg. "
-              "It will force the app client to send the serialized personal msg to be sent in chunk. "
-              "As the maximum chunk size is 255 bytes we will make this memo greater than 255 characters. "
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.")
+        personalmsg=(str_personal_msg)
     ).serialize()
 
     with client.sign_personal_msg(path=path, personalmsg=personalmsg):
         scenario_navigator.review_approve()
+
+    personalmsg = SIGN_MAGIC + str(utf8_strlen(str_personal_msg)).encode() + personalmsg
 
     response = client.get_async_response().data
     _, der_sig, _ = unpack_sign_personal_msg_response(response)
